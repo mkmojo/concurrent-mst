e4ba421524d354b1ee5f23a13133c28a42523a8b
Jeremy Warner <jeremywrnr@gmail.com>
2015-12-01 19:05:47 -0500

    mergin animation into jpanel

--- UI.java
+++ UI.java
@@ -7,11 +7,85 @@
 
 import java.awt.*;
 import java.awt.event.*;
-import java.io.*;
-import javax.swing.*;
+import java.io.*; import javax.swing.*;
 import java.util.*;
 import java.lang.*;
 import java.util.concurrent.ConcurrentSkipListSet;
+
+// Class Animation is the one really complicated sub-pane of the user interface.
+//
+class Animation extends JPanel {
+    private static final int width = 512;      // canvas dimensions
+    private static final int height = 512;
+    private static final int dotsize = 6;
+    private static final int border = dotsize;
+    private final Surface s;
+
+    // The next two routines figure out where to render the dot
+    // for a point, given the size of the animation panel and the spread
+    // of x and y values among all points.
+    //
+    private int xPosition(int x) {
+        return (int)
+            (((double)x-(double)s.getMinx())*(double)width
+                /((double)s.getMaxx()-(double)s.getMinx()))+border;
+    }
+    private int yPosition(int y) {
+        return (int)
+            (((double)s.getMaxy()-(double)y)*(double)height
+                /((double)s.getMaxy()-(double)s.getMiny()))+border;
+    }
+
+    // The following method is called automatically by the graphics
+    // system when it thinks the Animation canvas needs to be
+    // re-displayed.  This can happen because code elsewhere in this
+    // program called repaint(), or because of hiding/revealing or
+    // open/close operations in the surrounding window system.
+    //
+    public void paintComponent(final Graphics g) {
+        final Graphics2D g2 = (Graphics2D) g;
+
+        super.paintComponent(g);    // clears panel
+        s.forAllEdges(new Surface.EdgeRoutine() {
+            public void run(int x1, int y1, int x2, int y2, boolean bold) {
+                if (bold) {
+                    g2.setPaint(Color.red);
+                    g2.setStroke(new BasicStroke(4));
+                } else {
+                    g2.setPaint(Color.gray);
+                    g2.setStroke(new BasicStroke(1));
+                }
+                g.drawLine(xPosition(x1), yPosition(y1),
+                           xPosition(x2), yPosition(y2));
+            }
+        });
+        s.forAllPoints(new Surface.PointRoutine() {
+            public void run(int x, int y) {
+                g2.setPaint(Color.blue);
+                g.fillOval(xPosition(x)-dotsize/2, yPosition(y)-dotsize/2,
+                           dotsize, dotsize);
+            }
+        });
+    }
+
+    // UI needs to call this routine when point locations have changed.
+    //
+    public void reset() {
+        repaint();      // Tell graphics system to re-render.
+    }
+
+    // Constructor
+    //
+    public Animation(Surface S) {
+        setPreferredSize(new Dimension(width+border*2, height+border*2));
+        setBackground(Color.white);
+        setForeground(Color.black);
+        s = S;
+        reset();
+    }
+}
+
+
 class UI extends JPanel {
     private final Coordinator coordinator;
     private final Surface surface;
